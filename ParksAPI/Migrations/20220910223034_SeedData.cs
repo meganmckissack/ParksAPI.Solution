34bcb5cc683d85e9887829e778c54c04ad830027
Megan McKissack <meganmckissack@gmail.com>
2022-09-10 15:33:29 -0700

    create seed data in db context and create db migrations

---
+++ ParksAPI/Migrations/20220910223034_SeedData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksAPI.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "City", "Feature", "ParkName", "State", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Coos Bay", "yurts", "Bullards Beach", "Oregon", "State" },
+                    { 2, "The Dalles", "fishing", "Cottonwood Canyon", "Oregon", "State" },
+                    { 3, "Astoria", "swimming", "Fort Stevens State Park", "Oregon", "State" },
+                    { 4, "Crater Lake", "biking", "Crater Lake", "Oregon", "National" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4);
+        }
+    }
+}
--- ParksAPI/Models/ParksAPIContext.cs
+++ ParksAPI/Models/ParksAPIContext.cs
@@ -10,5 +10,17 @@ namespace ParksAPI.Models
         }
 
         public DbSet<Park> Parks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+          builder.Entity<Park>()
+            .HasData(
+              new Park { ParkId = 1, ParkName = "Bullards Beach", Type = "State", City = "Coos Bay", State = "Oregon", Feature = "yurts"},
+              new Park { ParkId = 2, ParkName = "Cottonwood Canyon", Type = "State", City = "The Dalles", State = "Oregon", Feature = "fishing"},
+              new Park { ParkId = 3, ParkName = "Fort Stevens State Park", Type = "State", City = "Astoria", State = "Oregon", Feature = "swimming"},
+              new Park { ParkId = 4, ParkName = "Crater Lake", Type = "National", City = "Crater Lake", State = "Oregon", Feature = "biking"}
+            );
+
+        }
     }
 }
